86f39c2e61d366792ccaeef7b1f04c7df5c62e37
Vlad <mr.6990@mail.ru>
2023-02-14 11:45:40 +0300

    Сверстал первую страницу, вывел список пользователей и реализовал лодер

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="list__item">
+    <div class="left">
+      <div class="left__name">
+        <div class="left__name--title mr-5">
+          ФИО:    
+        </div>  
+        <div class="left__name__user">
+          {{name}}
+        </div>
+      </div>
+      <div class="left__name">
+        <div class="left__name--title mr-5">
+            Город:
+        </div>
+        <div class="left__name__user">
+            {{city}}
+        </div>
+      </div>
+      <div class="left__name">
+        <div class="left__name--title mr-5">
+            Компания:
+        </div>
+        <div class="left__name__user">
+            {{company}}
+        </div>
+      </div>
+    </div>
+    <div class="right">
+        Подробнее
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['name', 'city', 'company']
+}
+</script>
+<style lang="scss" scoped>
+  .list__item {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    margin-bottom: 30px;
+  }
+  .left {
+    &__name {
+      display: flex;
+      &--title {
+        color: #A3A3A3;
+      }
+      &__user {
+        font-weight: bold;
+      }
+    }
+  }
+  .mr-5 {
+    margin-right: 10px;
+  }
+@media (max-width: 768px) {
+  .list__item {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  .left {
+    margin-bottom: 7px;
+  }
+}
+</style>
--- src/views/ListUsers.vue
+++ src/views/ListUsers.vue
@@ -7,14 +7,58 @@
           Список пользователей
         </div>
       </div>
+      <div class="loader" v-if="this.loading === true">
+        <CirclesLoader />
+      </div>
+      <UserInfo 
+        v-else
+        v-for="item in items[0]" 
+        :key="item"
+        :name="item.name"
+        :city="item.address.city"
+        :company="item.company.name"
+      />
+      <div class="footer" v-if="items[0] !== undefined">
+        <div class="footer__users">
+          Найдено {{ items[0].length }} пользователей
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script >
+import CirclesLoader from '../components/CirclesLoader.vue'
 import UserSorting from '../components/UsersSorting.vue'
+import UserInfo from '../components/UserInfo.vue'
 export default {
-  components: {UserSorting}
+  components: {UserSorting, UserInfo,CirclesLoader},
+  data() {
+    return {
+      items: this.$store.getters.GET_ITEMS,
+      loading: false
+    }
+  },
+  methods: {
+    requestServer() {
+      this.loading = true
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+          this.setResult(data)
+        })
+    },
+    setResult(data) {
+      this.items.push(data)
+      this.loading = false
+    }
+  },
+  mounted() {
+    this.requestServer()
+    console.log(this.items);
+  }
 }
 </script>
 <style lang="scss" scoped>
@@ -22,12 +66,25 @@ export default {
   display: flex;
 }
 .container {
-  margin: 20px 0 0 35px;
+  padding: 20px 35px 0 35px;
 }
 .list {
   &__title {
     font-size: 20px;
     font-weight: bold;
+    margin-bottom: 26px;
+  }
+}
+.footer {
+  font-weight: bold;
+  &__users {
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+@media (max-width: 425px) {
+  .container {
+    padding: 5px;
   }
 }
 </style>
